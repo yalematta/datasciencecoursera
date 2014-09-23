a270ba801b9f74d183227d9a0ed93ca8b501f013
yalisme <layale.matta@live.co.uk>
2014-09-23 22:25:35 +0300

    completed 1 - Intro & Architecture

---
+++ first-attempt/Assets/Scripts/MyDebugDemo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyDebugDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Starting...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Updating!");
+        transform.Translate(Vector3.forward * Time.deltaTime);
+	}
+}
